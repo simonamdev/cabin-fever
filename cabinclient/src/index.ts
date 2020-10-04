62acf2030d52d8352271f8b492b7010e84e37df9
Simon Agius Muscat <simon@agius-muscat.net>
2020-10-04 21:00:32 +0200

    Setup basic BE and FE to talk to each other

--- cabinclient/src/index.ts
+++ cabinclient/src/index.ts
@@ -1,3 +1,13 @@
 const message: string = "Hello, World!";
 
 console.log(message);
+
+setInterval(async () => {
+    const response = await fetch('http://localhost:8080', {
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+    });
+    const text = await response.text();
+    document.getElementById('root').innerText = text;
+}, 1000)
--- cabinserver/main.go
+++ cabinserver/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	message := fmt.Sprintf("The time now is %s", time.Now().String())
+	log.Println(message)
+	// Allow cors
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, message)
 }
 
 // Simplest web server example, taken from https://golang.org/doc/articles/wiki/
