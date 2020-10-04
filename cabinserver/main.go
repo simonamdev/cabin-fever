d27db7085a4da60e71e8d2d7678589cb46389301
Simon Agius Muscat <simon@agius-muscat.net>
2020-10-04 19:40:39 +0200

    Added modd for hot reload

--- cabinserver/main.go
+++ cabinserver/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+// Simplest web server example, taken from https://golang.org/doc/articles/wiki/
 func main() {
-	log.Println("Hello World")
+	http.HandleFunc("/", handler)
+	log.Println("Starting server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
