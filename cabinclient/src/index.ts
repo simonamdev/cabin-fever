3b989431e2b1ffb6d1cafc5dab9be6515567487c
Simon Agius Muscat <simon@agius-muscat.net>
2020-11-16 21:45:49 +0100

    Visualise game state using PIXIjs

--- cabinclient/src/index.ts
+++ cabinclient/src/index.ts
@@ -1,32 +1,43 @@
-// HTTP Method
-/*
-setInterval(async () => {
-    const response = await fetch('http://localhost:8080', {
-        headers: {
-            'Content-Type': 'text/plain'
-        },
-    });
-    const text = await response.text();
-    document.getElementById('root').innerText = text;
-}, 5000);
-*/
+import * as PIXI from 'pixi.js'
 
-// WS Method
-const testws = new WebSocket("ws://localhost:8080/ws");
+const app = new PIXI.Application();
 
-// On open, send the date every 1 second
-testws.onopen = (e) => {
-    setInterval(() => {
-        const date = new Date();
-        console.log(`Sending date: ${date} `);
-        testws.send(date.toISOString())
-    }, 1000)
+document.body.appendChild(app.view);
+
+const graphics = new PIXI.Graphics();
+
+graphics.beginFill(0xFFFF00);
+graphics.lineStyle(5, 0xFF0000);
+graphics.drawRect(0, 0, 300, 300);
+graphics.pivot.x = 150;
+graphics.pivot.y = 150;
+graphics.position.x = 300;
+graphics.position.y = 300;
+app.stage.addChild(graphics);
+
+
+interface PlayerPosition {
+    x: number;
+    y: number;
 }
 
-// On receive, update the DOM
-testws.onmessage = (ev) => {
-    document.getElementById('rootws').innerText = `WS Data received: ${ev.data}`;
-}
+const playerPosition: PlayerPosition = { x: 0, y: 0 };
+
+let text = new PIXI.Text(`X: ${playerPosition.x} Y: ${playerPosition.y}`, { fontFamily: 'Arial', fontSize: 24, fill: 0xff1010, align: 'center' });
+text.anchor.x = 0.5;
+app.stage.addChild(text);
+
+// Listen for frame updates
+app.ticker.add(() => {
+    graphics.rotation += 0.01;
+    // Update position
+    graphics.x = playerPosition.x;
+    graphics.y = playerPosition.y;
+    text.x = playerPosition.x;
+    text.y = playerPosition.y
+    text.text = `X: ${playerPosition.x} Y: ${playerPosition.y}`
+    text.updateText(true);
+});
 
 enum Direction {
     Up = 'UP',
@@ -35,15 +46,7 @@ enum Direction {
     Right = 'RIGHT'
 }
 
-interface PlayerPosition {
-    x: number;
-    y: number;
-}
-
-const position: PlayerPosition = { x: 0, y: 0 };
 const ws = new WebSocket("ws://localhost:8080/game");
-const body = document.getElementById('body');
-const gameRoot = document.getElementById('game');
 
 const getDir = (key: string): Direction => {
     if (key === 'w') {
@@ -61,10 +64,13 @@ ws.onopen = (e) => {
     console.log('Connection Setup');
 
     // Setup keyboard events
-    body.onkeydown = (e) => {
-        console.log(e.key);
-        if (['w', 'a', 's', 'd'].includes(e.key)) {
-            ws.send(getDir(e.key));
+    const body = document.getElementById('body');
+    if (body) {
+        body.onkeydown = (e) => {
+            console.log(e.key);
+            if (['w', 'a', 's', 'd'].includes(e.key)) {
+                ws.send(getDir(e.key));
+            }
         }
     }
 }
@@ -73,7 +79,9 @@ ws.onmessage = (e) => {
     // When we receive the updated position, update the client side state
     const { data } = e;
     const position: PlayerPosition = JSON.parse(data); // Ideally we do validation here
-    gameRoot.innerText = JSON.stringify(position);
+    // gameRoot.innerText = JSON.stringify(position);
+    playerPosition.x = position.x * 10;
+    playerPosition.y = position.y * 10;
 }
 
 
